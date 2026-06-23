a7cbb42ae18fcc1adad3a217e8278ba7be6c8f83
OPWNMK <1780963937@qq.com>
2026-06-23 14:36:37 +0800

    Initial commit

---
+++ hash/字母异位词分组.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> tmp_res;
+        for(const auto&word:strs)
+        {
+            string key = word;
+            sort(key.begin(),key.end());
+            tmp_res[key].emplace_back(word);
+        }
+        for(const auto &value:tmp_res)
+        {
+            ans.push_back(value.second);
+        }
+        return ans;
+    }
+};
