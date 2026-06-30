6d2d6fb0a271eb95d34dfd08f272cd0f02d13922
OPWNMK <1780963937@qq.com>
2026-06-30 17:36:11 +0800

    fifth day

--- substring/最小覆盖子串.cpp
+++ substring/最小覆盖子串.cpp
@@ -1,10 +1,25 @@
 #include "commen.h"
 
-class Solution {
+class Solution
+{
 public:
-    string minWindow(string s, string t) {
-        if(s.size() < t.size())
+    string minWindow(string s, string t)
+    {
+        if (s.size() < t.size())
             return "";
-        
+        int t_size = t.size();
+        int s_size = s.size();
+        unordered_map<char, int> t_count;
+        for (int i = 0; i < t_size; ++i)
+        {
+            ++t_count[t[i]];
+        }
+        int left = 0;
+        int right = 0;
+        for (right = left; right < s_size; ++right)
+        {
+            
+        }
+        return s.substr(left,right - left);
     }
 };
